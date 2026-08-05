fa46ec8ae183dc2d95950173db4323f505345089
JangTV <ronjay.1204@gmail.com>
2026-08-05 16:41:50 +0800

    Fix background image URL for correct rendering in the UI

--- sia.py
+++ sia.py
@@ -263,7 +263,7 @@ if not st.session_state['logged_in']:
         left: 0;
         width: 100%;
         height: 100%;
-        background-image: url("https://github.com/RJA24/abra_sia_2026/blob/main/img/Abra%20(2).png?raw=true");
+        background-image: url("https://github.com/RJA24/abra_sia_2026/blob/main/Abra%20(2).png?raw=true");
         background-size: cover;
         background-position: center;
         opacity: 0.3; /* Sets the image to 30% opacity */
